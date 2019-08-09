2e2fc00002d64b82459a6a761706485c6c7b4d58
Sohrab Taee <sohrab.taee@gmail.com>
2019-08-09 11:53:15 +0300

    Added onboarding functionality, demo, and lumo styles.

--- src/vcf-onboarding.js
+++ src/vcf-onboarding.js
@@ -1,15 +1,57 @@
 import { html, PolymerElement } from '@polymer/polymer/polymer-element';
+import { GestureEventListeners } from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin';
 import { ElementMixin } from '@vaadin/vaadin-element-mixin';
+import '@vaadin/vaadin-dialog';
+import '@vaadin/vaadin-button';
 
-class VcfOnboarding extends ElementMixin(ThemableMixin(PolymerElement)) {
+/**
+ * The following selectors are available for styling:
+ *
+ * Selector | Description
+ * ----------------|----------------
+ * `[part='step-container']` | The element that wraps all the steps
+ * `[part='step']` | The element that wraps the contents and the button of each step
+ * `[part='step-content']` | The element that wraps the contents of each step
+ * `[part='step-button']` | The button of each step
+ * `.active-step` | The CSS class added to the current step the user is viewing
+ *
+ * How to provide and style content of the steps:
+ *  Add a style tag with 'onboading-styles' attribute and place your styles inside it.
+ *  Wrap the content of each step in an element with 'onboarding-step' attribute
+ *  and add the button text in 'button-text' attribute of the element.
+ *
+ * ```html
+ * <style onboarding-styles></style>
+ * <div onboarding-step button-text="Next Step">
+ *   <h1>First Step</h1>
+ * </div>
+ * <div onboarding-step button-text="Next Step">
+ *  <h1>Second Step</h1>
+ * </div>
+ * <div onboarding-step button-text="Finish!">
+ *   <h1>Third Step</h1>
+ * </div>
+ * ```
+ */
+class VcfOnboarding extends ElementMixin(ThemableMixin(GestureEventListeners(PolymerElement))) {
   static get template() {
     return html`
-      <style>
-        :host {
-          display: block;
-        }
-      </style>
+      <vaadin-dialog id="onboarding-dialog" theme="full-screen-on-mobile" no-close-on-outside-click no-close-on-esc>
+        <template>
+          <style inner-h-t-m-l="[[styles.innerHTML]]"></style>
+          <div part="steps-container" on-track="handleTrack">
+            <template is="dom-repeat" items="[[steps]]">
+              <div part="step" class$="[[_getStepClasses(index, activeStep)]]">
+                <div part="step-content" inner-h-t-m-l="[[item.innerHTML]]"></div>
+                <vaadin-button part="step-button" theme="success primary" on-click="nextStep"
+                  >[[_getButtonText(item)]]</vaadin-button
+                >
+              </div>
+            </template>
+          </div>
+        </template>
+      </vaadin-dialog>
     `;
   }
 
@@ -22,7 +64,95 @@ class VcfOnboarding extends ElementMixin(ThemableMixin(PolymerElement)) {
   }
 
   static get properties() {
-    return {};
+    return {
+      steps: {
+        type: Array,
+        value: () => []
+      },
+      buttons: {
+        type: Array,
+        value: () => []
+      },
+      styles: {
+        type: Object,
+        value: () => {}
+      },
+      activeStep: {
+        type: Number,
+        value: 0
+      }
+    };
+  }
+
+  ready() {
+    super.ready();
+
+    if (!localStorage.getItem('vcf-onboarding')) {
+      this.openDialog();
+    }
+
+    const stepElements = this.querySelectorAll('[onboarding-step]');
+    this.steps = [...stepElements];
+
+    const styles = this.querySelector('[onboarding-styles]');
+    this.styles = styles;
+
+    this.innerHTML = '';
+  }
+
+  _getStepClasses(i, activeStep) {
+    if (i === activeStep) {
+      return 'active-step';
+    }
+    return '';
+  }
+
+  _getButtonText(step) {
+    return step.getAttribute('button-text');
+  }
+
+  nextStep() {
+    if (this.activeStep < this.steps.length - 1) {
+      this.activeStep += 1;
+    } else {
+      this.closeDialog();
+    }
+  }
+
+  openDialog() {
+    this.$['onboarding-dialog'].opened = true;
+  }
+
+  closeDialog() {
+    this.$['onboarding-dialog'].opened = false;
+    localStorage.setItem('vcf-onboarding', 'visited');
+  }
+
+  handleTrack(e) {
+    switch (e.detail.state) {
+      case 'start':
+        this.trackStart = {
+          x: e.detail.x,
+          y: e.detail.y
+        };
+        break;
+      case 'end':
+        if (this.trackStart) {
+          const deltaX = e.detail.x - this.trackStart.x;
+          const deltaY = e.detail.y - this.trackStart.y;
+
+          if (Math.abs(deltaX) > Math.abs(deltaY)) {
+            if (deltaX > 0 && this.activeStep > 1) {
+              this.activeStep = this.activeStep - 1;
+            } else if (deltaX < 0 && this.activeStep < 3) {
+              this.activeStep = this.activeStep + 1;
+            }
+          }
+
+          this.trackStart = undefined;
+        }
+        break;
+    }
   }
 }
 
--- theme/lumo/vcf-onboarding-styles.js
+++ theme/lumo/vcf-onboarding-styles.js
@@ -1,11 +1,28 @@
-const theme = document.createElement('dom-module');
-theme.id = 'vcf-onboarding-lumo';
-theme.setAttribute('theme-for', 'vcf-onboarding');
-theme.innerHTML = `
+import { html } from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-lumo-styles/color';
+import '@vaadin/vaadin-lumo-styles/spacing';
+import '@vaadin/vaadin-lumo-styles/sizing';
+import '@vaadin/vaadin-lumo-styles/style';
+import '@vaadin/vaadin-lumo-styles/typography';
+
+const template = html`
+  <dom-module id="my-dialog-overlay-styles" theme-for="vaadin-dialog-overlay">
     <template>
       <style>
-        :host {}
+        [part='content'] {
+          padding: 0;
+          height: 100%;
+        }
       </style>
     </template>
-  `;
-theme.register(theme.id);
+  </dom-module>
+  <dom-module id="vcf-onboarding-lumo" theme-for="vcf-onboarding">
+    <template>
+      <style>
+        
+      </style>
+    </template>
+  </dom-module>
+`;
+
+document.head.appendChild(template.content);
